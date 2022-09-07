7fccec653537a57b392fd9788d21d2381760e8d8
Nate <natestutte@protonmail.com>
2022-09-07 08:02:46 -0400

    Added check for repeat images

--- lambda_function.py
+++ lambda_function.py
@@ -3,6 +3,7 @@ import tweepy as tp
 import requests
 import boto3
 import tempfile
+import json
 
 # OtterAnHour Twitter Bot
 # By Nate Stutte
@@ -21,6 +22,13 @@ def lambda_handler(event, context):
     api.verify_credentials()
     print("Verified Credentials!")
 
+    # Load JSON file of used filenames
+    content_object = s3_resource.Object("otterpics", "used.json")
+    usedfilenames = json.loads(content_object.get()['Body'].read().decode('utf-8'))
+
+    if len(usedfilenames) > 200:
+        usedfilenames = []
+
     # ty https://stackoverflow.com/questions/59225939/get-only-file-names-from-s3-bucket-folder
     filenames = []
     result = s3_client.list_objects_v2(Bucket="otterpics")
@@ -28,7 +36,13 @@ def lambda_handler(event, context):
         files = item['Key']
         filenames.append(files)   #optional if you have more filefolders to got through.
 
-    img = random.choice(filenames)
+    while(1):
+        img = random.choice(filenames)
+        if img not in usedfilenames:
+            break
+    usedfilenames.append(img)
+    s3_client.put_object(Body=json.dumps(usedfilenames), Bucket="otterpics", Key="used.json")
+
     bucket = s3_resource.Bucket("otterpics")
     img_obj = bucket.Object(img)
     
